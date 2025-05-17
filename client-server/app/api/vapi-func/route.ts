b70c1127bcebca46303ce67307d44de469f5b3a8
Mohit Nagaraj <mohitnagaraj20@gmail.com>
2025-05-17 23:57:20 +0530

    Update route.ts

--- client-server/app/api/vapi-func/route.ts
+++ client-server/app/api/vapi-func/route.ts
@@ -21,19 +21,34 @@ export async function POST(req: Request) {
     const body = await req.json();
     console.log('Received vapi data:', body);
     const { message } = body;
-    console.log("message",message)
+    console.log("message", message)
     const function_call = message.toolCalls[0].function
-    console.log("function_call",function_call)
+    console.log("function_call", function_call)
     switch (function_call?.name) {
       case 'raiseCase': {
         const { symptom, duration } = function_call.arguments;
         const caseId = await createCase(symptom, duration);
 
         return new NextResponse(
-          JSON.stringify(caseId),
+          JSON.stringify({ result: { message: `Case raised successfully with ID: ${caseId}` } }),
           {
             status: 200,
             headers: CORS_HEADERS,
+          }
+        );
+      }
+
+      case 'getUserCases': {
+        const cases = await getUserCases("user_2wZbsW6bPlUMdpl88fNHlWphguY");
+        return new NextResponse(
+          JSON.stringify({
+            cases: cases
+          }),
+          {
+            status: 200,
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
           }
         );
       }
@@ -53,12 +68,12 @@ export async function POST(req: Request) {
   }
 }
 
-async function createCase(symptom: string, duration: string): Promise<number | null> {
+async function createCase(symptom: string, duration: string): Promise<string | null> {
   const { userId } = await auth();
   const inputText = `I have been feeling ${symptom} for ${duration}.`;
   let newuserId = userId;
-    if(!newuserId){
-      newuserId="user_2wZbsW6bPlUMdpl88fNHlWphguY"
+  if (!newuserId) {
+    newuserId = "user_2wZbsW6bPlUMdpl88fNHlWphguY"
   }
 
   try {
@@ -74,35 +89,65 @@ async function createCase(symptom: string, duration: string): Promise<number | n
     }
 
     const { id: caseId } = data;
+    console.log("CaseId",caseId)
 
-    const fastapiRes = await fetch(`${process.env.NEXT_PUBLIC_FASTAPI_URL}/predict`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ input_text: inputText }),
-    });
+    try{
+      const fastapiRes = await fetch(`https://q1xxcnb9-8000.inc1.devtunnels.ms/predict`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ input_text: inputText }),
+      });
+  
+      if (!fastapiRes.ok) throw new Error('FastAPI response failed');
+  
+      const fastapiData = await fastapiRes.json();
+      console.log('FastAPI response:', fastapiData);
+  
+      const triageRes = await fetch(`http://localhost:3000/api/triage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          labels: fastapiData.label,
+          summary: fastapiData.summary,
+          recommended_action: fastapiData.recommended_action,
+          risk_score: fastapiData.risk_score,
+          case_id: caseId,
+        }),
+      });
+  
+      if (!triageRes.ok) console.warn('Triage POST failed');
+  
+      return caseId;
+    }catch(e:any){
+      console.log("err3",e)
+      return null;
+    }
 
-    if (!fastapiRes.ok) throw new Error('FastAPI response failed');
-
-    const fastapiData = await fastapiRes.json();
-    console.log('FastAPI response:', fastapiData);
-
-    const triageRes = await fetch(`http://localhost:3000/api/triage`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        labels: fastapiData.label,
-        summary: fastapiData.summary,
-        recommended_action: fastapiData.recommended_action,
-        risk_score: fastapiData.risk_score,
-        case_id: caseId,
-      }),
-    });
-
-    if (!triageRes.ok) console.warn('Triage POST failed');
-
-    return caseId;
+    
   } catch (err) {
     console.error('createCase error:', err);
     return null;
   }
+}
+
+async function getUserCases(userId: string) {
+
+  const { data, error } = await supaclient
+    .from('cases')
+    .select(`*,
+                triage_results (
+                    id,
+                    labels,
+                    summary,
+                    recommended_action
+                )`)
+    .eq("user_id", userId)
+
+  if (error) {
+    console.error('Error fetching cases:', error);
+    return 'Failed to fetch cases';
+  }
+
+  return data;
+
 }
